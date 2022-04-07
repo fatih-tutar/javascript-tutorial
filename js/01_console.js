d0b56611a9ca8fd1988e36437e2a79725d4b15ef
fatih-tutar <fatihtutar93@gmail.com>
2022-04-07 14:21:20 +0300

    Variables

--- js/01_console.js
+++ js/01_console.js
@@ -1,2 +1,4 @@
 alert("Hello World")
 console.log('1234')
+console.log(true)
+
---
+++ js/02_variable.js
@@ -0,0 +1,10 @@
+var name
+
+var x = 'fatih'
+console.log(x)
+
+let fullname = "Fatih Tutar"
+console.log(fullname)
+
+const age = 28 // Can not be changed
+console.log(age)
