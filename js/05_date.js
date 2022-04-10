215f962a253010f3d884cae88256c5ace22c5020
fatih-tutar <fatihtutar93@gmail.com>
2022-04-10 18:33:03 +0300

    Date Object

---
+++ js/05_date.js
@@ -0,0 +1,18 @@
+// Date Object
+var d = new Date();
+console.log(d)
+
+// Set Methods
+d.setFullYear(1996)
+d.setMonth(10)
+d.setDate(25)
+
+// Get Methods
+console.log(d.getFullYear())
+console.log(d.getMonth())
+console.log(d.getDate())
+console.log(d.getHours())
+console.log(d.getMinutes())
+console.log(d.getSeconds())
+console.log(d.getMilliseconds())
+console.log(d.getDay())
