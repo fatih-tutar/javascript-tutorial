2c4b32da5c197a60b0aa9a71761613b7fd36d30a
fatih-tutar <fatihtutar93@gmail.com>
2022-04-10 17:59:58 +0300

    Variable Types

---
+++ js/03_variable_types.js
@@ -0,0 +1,44 @@
+// Variables Types
+
+// Primitive Types
+
+// undefined
+var x;
+console.log(x)
+console.log(typeof x)
+
+// null
+var y = null;
+console.log(typeof y)
+
+// string
+var firstName = 'fatih'
+console.log(typeof firstName)
+
+// number
+var age = 28
+console.log(typeof age)
+
+// boolean
+var isOnline = true
+console.log(typeof isOnline)
+
+//Reference Types - Objects
+
+// Array
+var cars = ['Honda','Volvo','Toyota']
+console.log(typeof cars)
+
+// Object
+var person = {
+    name : 'Fatih',
+    age : 28
+}
+console.log(typeof person)
+
+// function
+var isAlive = function(){
+    return 0;
+}
+console.log(typeof isAlive)
+
