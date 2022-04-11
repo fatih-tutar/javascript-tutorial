dd7b9dd669c9c98d93a6e3756fb99383b45596f2
fatih-tutar <fatihtutar93@gmail.com>
2022-04-11 11:33:23 +0300

    Condition Statements

---
+++ js/10_ifelse.js
@@ -0,0 +1,17 @@
+// Condition Statements
+
+// if else
+var firstName = 'Fatih'
+var age = 28
+var drivingLicence = true
+
+if(firstName == 'Fatih'){
+    console.log('My name is Fatih')
+}
+
+if(age > 30){
+    console.log('Your age is bigger than 30')
+}else{
+    console.log('Your age is smaller than 30')
+}
+
