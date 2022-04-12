fa9e5b81e872fce1850b28059c267632fa7894ac
fatih-tutar <fatihtutar93@gmail.com>
2022-04-12 15:28:29 +0300

    DOM Intro

---
+++ js/19_dom_intro.js
@@ -0,0 +1,30 @@
+let val 
+val = window.document
+console.log(val)
+
+val = document.all
+console.log(val)
+
+val = document.all.length
+console.log(val)
+
+val = document.all[5]
+console.log(val)
+
+val = document.head
+console.log(val)
+
+val = document.body
+console.log(val)
+
+val = document.URL
+console.log(val)
+
+val = document.images
+console.log(val)
+
+val = document.title
+console.log(val)
+
+val = document.forms
+console.log(val)
