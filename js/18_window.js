e41e66931cd6642e93460de2a74822997ed21129
fatih-tutar <fatihtutar93@gmail.com>
2022-04-12 15:17:58 +0300

    Window

---
+++ js/18_window.js
@@ -0,0 +1,27 @@
+// Window Object
+let val
+var a = 10
+function b(){
+    return 0
+}
+// alert
+alert('Hello')
+
+//prompt
+var c = prompt('Enter a number')
+console.log(c)
+
+//confirm
+var d = confirm('Are you sure?')
+console.log(d)
+if(d == true){
+    console.log('ok')
+}else{
+    console.log('canceled')
+}
+
+val = window
+console.log(val)
+
+val = window.location
+console.log(val)
