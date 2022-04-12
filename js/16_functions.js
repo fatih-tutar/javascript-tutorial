996988481fdbc068b4cbfae65e2f5621629b5b46
fatih-tutar <fatihtutar93@gmail.com>
2022-04-12 14:44:38 +0300

    Functions

---
+++ js/16_functions.js
@@ -0,0 +1,16 @@
+// Functions
+
+function square(number){
+    return Math.pow(number,2)
+}
+
+let a = square(2)
+console.log(a)
+let b = square(4)
+console.log(b)
+
+function sum(num1, num2){
+    return square(num1) + square(num2)
+}
+
+console.log(sum(2,4))
