a9f8bdab8df889aa7a57e625d92208d945f749ee
fatih-tutar <fatihtutar93@gmail.com>
2022-04-12 17:48:33 +0300

    Local and Session Storage

---
+++ js/28_storages.js
@@ -0,0 +1,7 @@
+// LOCAL STORAGE
+const data = localStorage.setItem('name','Mustafa')
+console.log(localStorage)
+
+// SESSION STORAGE
+const country = sessionStorage.setItem('country','Turkey')
+console.log(sessionStorage)
