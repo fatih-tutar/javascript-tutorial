3e143dd54503f7b44a6dacc0d4c2c503d5a9fac2
fatih-tutar <fatihtutar93@gmail.com>
2022-04-20 15:47:55 +0300

    OOP Intro

---
+++ js/37_oop.js
@@ -0,0 +1,18 @@
+let val
+
+let person = {
+    name : 'Fatih',
+    yearOfBirth : 2001
+}
+
+val = person
+
+console.log(typeof val)
+console.log(val)
+
+// array 
+let numbers = [10,20,30,40,50]
+val = numbers
+console.log(typeof val)
+console.log(val)
+
